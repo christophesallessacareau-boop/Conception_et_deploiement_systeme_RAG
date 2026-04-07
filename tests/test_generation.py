16eb567178bc2a59392aaed56ec5fa4bcbe88c6b
Christophe Salles <christophe.sallessacareau@gmail.com>
2026-04-07 13:55:35 +0200

    remove of test_generation.py

--- tests/test_generation.py
+++
@@ -1,9 +0,0 @@
-# test generation
-import numpy as np
-from main import rechercher
-
-def test_generation():
-    response = "Voici des événements à Toulouse"
-
-    assert isinstance(response, str)
-    assert len(response) > 0
